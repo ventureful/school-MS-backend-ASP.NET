2a88f5287afd0bebe5e2ff93cd7d2a369c0b560f
ventureful <hades0122@outlook.com>
2023-05-30 04:40:28 -0400

    added migrations

---
+++ Migrations/20230529211951_Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace StudentTodo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_student",
+                columns: table => new
+                {
+                    student_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    student_name = table.Column<string>(type: "text", nullable: false),
+                    student_gender = table.Column<int>(type: "integer", nullable: false),
+                    student_title = table.Column<string>(type: "text", nullable: true),
+                    student_description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_student", x => x.student_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_student");
+        }
+    }
+}
